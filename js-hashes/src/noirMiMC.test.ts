2ac61534aed94cd60d6ee5257bf5476601872c3e
Sergey <mysteryon88@yandex.ru>
2024-03-20 02:09:12 +0300

    fmt

--- js-hashes/src/noirMiMC.test.ts
+++ js-hashes/src/noirMiMC.test.ts
@@ -2,22 +2,22 @@ import { Noir } from "@noir-lang/noir_js";
 import { BarretenbergBackend } from "@noir-lang/backend_barretenberg";
 import { CompiledCircuit } from "@noir-lang/types";
 import { compile, createFileManager } from "@noir-lang/noir_wasm";
-import { join, resolve } from 'path';
-import fs from 'fs/promises'; 
+import { join, resolve } from "path";
+import fs from "fs/promises";
 
 async function getCircuit() {
-  const basePath = resolve(join('./src/mimc'));
+  const basePath = resolve(join("./src/mimc"));
   const fm = createFileManager(basePath);
   const compiled = await compile(fm, basePath);
-  if (!('program' in compiled)) {
-    throw new Error('Compilation failed');
+  if (!("program" in compiled)) {
+    throw new Error("Compilation failed");
   }
   return compiled.program;
 }
 
 async function getCircuitFromFile(filePath: string): Promise<CompiledCircuit> {
   try {
-    const data = await fs.readFile(filePath, 'utf8');
+    const data = await fs.readFile(filePath, "utf8");
     const json = JSON.parse(data);
 
     return json as CompiledCircuit;
@@ -28,8 +28,8 @@ async function getCircuitFromFile(filePath: string): Promise<CompiledCircuit> {
 
 describe("GetNoirMiMC", () => {
   let circuit: CompiledCircuit;
-  let backend : BarretenbergBackend;
-  let noir : Noir;
+  let backend: BarretenbergBackend;
+  let noir: Noir;
 
   afterEach(async () => {
     await backend.destroy();
@@ -46,8 +46,8 @@ describe("GetNoirMiMC", () => {
   });
 
   test("test precompile", async () => {
-    const basePath = resolve(join('./src/mimc/target/mimc.json'));
-    circuit = await getCircuitFromFile(basePath)
+    const basePath = resolve(join("./src/mimc/target/mimc.json"));
+    circuit = await getCircuitFromFile(basePath);
     backend = new BarretenbergBackend(circuit, { threads: 8 });
     noir = new Noir(circuit, backend);
 
